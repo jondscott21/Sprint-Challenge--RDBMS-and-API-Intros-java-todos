95505b3b7b3a756d33ad65b012c7b77b5d65ddb9
Jonathan Scott <jonathan.scott@gmail.com>
2019-09-13 11:03:05 -0700

    closer to mvp

--- todos/src/main/java/com/lambdaschool/todos/controller/TodoController.java
+++ todos/src/main/java/com/lambdaschool/todos/controller/TodoController.java
@@ -1,5 +1,25 @@
 package com.lambdaschool.todos.controller;
 
+import com.lambdaschool.todos.model.Todo;
+
+import com.lambdaschool.todos.service.TodoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/todos")
 public class TodoController
 {
+    private TodoService todoService;
+
+    @PutMapping(value = "/todo/{userid}")
+    public ResponseEntity<?> updateTodo(@RequestBody
+                                                Todo updateTodo, @PathVariable
+                                                long userid)
+    {
+        todoService.update(updateTodo, userid);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
